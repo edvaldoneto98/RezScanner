f744e1f3648c87212c83942ca4d3e10c2e0f0c79
Neto <netoedvaldo98@gmail.com>
2026-02-13 11:02:34 -0300

    Feat/DomainException

---
+++ src/modelException/DomainException.java
@@ -0,0 +1,13 @@
+package src.modelException;
+
+public class DomainException extends RuntimeException {
+    private static final long sertalVersionUID = 1L;
+
+    public DomainException(String msg) {
+        super(msg);
+    }
+
+    
+    
+    
+}
